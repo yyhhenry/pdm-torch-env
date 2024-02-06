a0ba864c643b4675b859c4b88f03d7004074d7dd
yyhhenry <yyhhenry@foxmail.com>
2024-02-06 18:08:47 +0800

    Update environment test script

--- env_test.py
+++ env_test.py
@@ -1,20 +1,19 @@
 import sys
 
-print("python == ", sys.version)
-
 import numpy as np
+import torch
+import torchvision
+from torch import Tensor
+
+print("python == ", sys.version)
 
 print("numpy ==", np.__version__)
 
-import torch
-from torch import Tensor
-
 print("torch == ", torch.__version__)
+
+print("torchvision == ", torchvision.__version__)
 
 x = Tensor([1, 2, 3]).cuda()
 y = Tensor([1, 4, 9]).cuda()
 assert bool((x * x == y).all().item())
-
-import torchvision
-
-print("torchvision == ", torchvision.__version__)
+print("torch.cuda works!")
