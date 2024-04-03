89a286d6f9d86b6db92e37101a61cb0092765424
yyhhenry <yyhhenry@foxmail.com>
2024-04-03 13:22:59 +0800

    Back to torch 2.1 and update pdm

--- env_test.py
+++ env_test.py
@@ -4,13 +4,15 @@ import torch
 import torchvision
 from torch import Tensor
 
-print("python == ", sys.version)
+print("python", sys.version)
 
-print("torch == ", torch.__version__)
+print("torch", torch.__version__)
 
-print("torchvision == ", torchvision.__version__)
+print("torchvision", torchvision.__version__)
+
+print("cuda", torch.cuda.is_available())
 
 x = Tensor([1, 2, 3]).cuda()
 y = Tensor([1, 4, 9]).cuda()
 assert bool((x * x == y).all().item())
-print("torch.cuda works!")
+print("torch.cuda works")
